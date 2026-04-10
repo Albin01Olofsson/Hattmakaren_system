2527f09f0baf0cad5271da86e8dce7dac632b0c7
kanatdavid <david.kanat@hotmail.com>
2026-04-10 13:59:04 +0200

    Ändrade i DBRepo IEnumerable till List

--- BL/Services/ProduktService.cs
+++ BL/Services/ProduktService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using BL.Interfaces;
+using DAL.Intefaces;
+using Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,24 @@ using System.Threading.Tasks;
 
 namespace BL.Services
 {
-    internal class ProduktService
+    public class ProduktService : IProduktService
     {
+        private readonly IProduktRepo prodRepo;
+        public ProduktService(IProduktRepo repository)
+        {
+            prodRepo = repository;
+        }
+
+        public List<Produkt> GetProdukt() => prodRepo.GetAll();
+
+        public Produkt GetProduktId(int id) => prodRepo.GetById(id);
+
+        public void AddProdukt(Produkt p) => prodRepo.Add(p);
+
+        public void UpdateProdukt(Produkt p) => prodRepo.Update(p);
+        public void DeleteProdukt(int id) => prodRepo.Delete(id);
+        public void SaveProdukt() => prodRepo.Save();
+
+
     }
 }

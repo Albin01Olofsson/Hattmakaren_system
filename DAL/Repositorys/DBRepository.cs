908fcf8dacef3f1524ff0126f067c5c4509080ce
Albin01Olofsson <albin01olofsson@gmail.com>
2026-04-10 10:24:16 +0200

    Repository<T>

---
+++ DAL/Repositorys/DBRepository.cs
@@ -0,0 +1,58 @@
+﻿using DAL.Intefaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL.Repositorys
+{
+    public class DBRepository<T> : IRepository<T> where T : class
+    {
+        private readonly DBcontext _context;
+        private readonly DbSet<T> _dbSet;
+
+        public DBRepository(DBcontext dbContext)
+        {
+            _context = dbContext;
+            _dbSet = _context.Set<T>();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return _dbSet.ToList();
+        }
+
+        // READ: Hittar en specifik rad via ID
+        public T GetById(int id)
+        {
+            return _dbSet.Find(id);
+        }
+
+        // CREATE: Lägger till ett objekt i kön
+        public void Add(T entity)
+        {
+            _dbSet.Add(entity);
+        }
+
+        // UPDATE: Informerar EF om att objektet har ändrats
+        public void Update(T entity)
+        {
+            _dbSet.Attach(entity);
+            _context.Entry(entity).State = EntityState.Modified;
+        }
+
+        // DELETE: Tar bort objektet
+        public void Delete(int id)
+        {
+            T existing = _dbSet.Find(id);
+            if (existing != null)
+            {
+                _dbSet.Remove(existing);
+            }
+        }
+
+        // COMMIT: Sparar allt till databasen
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+
+    }
+}

136bb500e7d4529b4bc07493b2e2ee8bc3c7dcf8
kanatdavid <david.kanat@hotmail.com>
2026-04-10 12:31:34 +0200

    f

---
+++ DAL/Repositorys/ProduktRepo.cs
@@ -0,0 +1,19 @@
+﻿using DAL.Intefaces;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repositorys
+{
+    public class ProduktRepo : DBRepository<Produkt>, IProduktRepo
+    {
+        public DBcontext _context;
+        public ProduktRepo(DBcontext context) : base(context)
+        {
+            _context = context;
+        }
+    }
+}

4947d5b280f6044660dde22aa9179dffeb25720f
kanatdavid <david.kanat@hotmail.com>
2026-04-13 20:17:01 +0200

    a

--- WpfApp1/ViewModels/OrderVM.cs
+++ WpfApp1/ViewModels/OrderVM.cs
@@ -26,6 +26,23 @@ namespace WpfApp1.ViewModels
 
         public int AntalOrders => OrderList.Count;
 
+        [ObservableProperty]
+        public DateTime? datumTillFilter;
+
+        [ObservableProperty]
+        public DateTime? datumFrånFilter;
+
+        [ObservableProperty]
+        public string orderStartareFilter = "Alla";
+
+        [ObservableProperty]
+        public string klarFilter = "Ospecificerat";
+
+        [ObservableProperty]
+        public string specialBeställningFilter = "Ospecificerat";
+
+
+
         public OrderVM(IOrderService s)
         {
             _service = s;
@@ -37,12 +54,52 @@ namespace WpfApp1.ViewModels
         {
             var sökResultat = _service.GetOrdersWithNavProps();
 
+            //Fylla orderlistan som visas på sökta ordrar, med filtreringen
+
+            //Sökstring
             if (!string.IsNullOrWhiteSpace(SökString))
             {
                 sökResultat = sökResultat.Where(
                     o => o.Kund.Namn.StartsWith(SökString, StringComparison.OrdinalIgnoreCase) || 
                     o.OrderID.ToString().StartsWith(SökString, StringComparison.OrdinalIgnoreCase)).ToList();
             }
+
+            //Datum från Filter
+            if (DatumFrånFilter.HasValue)
+            {
+                sökResultat = sökResultat.Where(o => o.Datum.Date >= DatumFrånFilter.Value.Date).ToList();
+            }
+
+            //Datum till Filter
+            if (DatumTillFilter.HasValue)
+            {
+                sökResultat = sökResultat.Where(o => o.Datum.Date <= DatumTillFilter.Value.Date).ToList();
+            }
+
+            //Order startare Filter
+            if (OrderStartareFilter != "Alla")
+            {
+                sökResultat = sökResultat.Where(o => o.StartadAv.Namn == OrderStartareFilter).ToList();
+            }
+
+            //Klar status Filter
+            if(KlarFilter == "Klar")
+            {
+                sökResultat = sökResultat.Where(o => o.Färdig == true).ToList();
+            }else if(KlarFilter == "Ej klar")
+            {
+                sökResultat = sökResultat.Where(o => o.Färdig == false).ToList();
+            }
+
+            //Specialbeställning Filter
+            //if(SpecialBeställning == "Ja")
+            //{
+            //    sökResultat = sökResultat.Where(o => o.IsSpecialBeställning == true).ToList();
+            //}else if(SpecialBeställning == "Nej")
+            //{
+            //    sökResultat = sökResultat.Where(o => o.IsSpecialBeställning == false).ToList();
+            //}
+
 
             OrderList = new ObservableCollection<Order>(sökResultat);
             OnPropertyChanged(nameof(AntalOrders));

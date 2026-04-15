b2e05efae4179d88196128ff605ac5d119e6f03c
Rostyslav Malanchuk <rostyslavmalanchuk15@gmail.com>
2026-04-15 12:41:56 +0200

    sparakund

--- BL/Interfaces/IKundService.cs
+++ BL/Interfaces/IKundService.cs
@@ -6,6 +6,15 @@ namespace BL.Interfaces
     {
         public List<Kund> HämtaAllaKunder();
 
+        Kund GetKundById(int id);
+
+        void AddKund(Kund kund);
+
+        void UpdateKund(Kund kund);
+
+        void DeleteKund(int id);
+
         public Kund GetByEmail(string email);
     }
+
 }
--- WpfApp1/Views1/CreateOrderPage.xaml.cs
+++ WpfApp1/Views1/CreateOrderPage.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows.Controls;
+﻿using BL.Interfaces;
+using BL.Services;
+using System.Windows;
+using System.Windows.Controls;
 using WpfApp1.ViewModels; // Se till att detta matchar mappen där din ViewModel ligger
 
 namespace WpfApp1.Views1
@@ -8,13 +11,25 @@ namespace WpfApp1.Views1
     /// </summary>
     public partial class CreateOrderPage : Page
     {
+        private readonly IKundService _kundService;
 
-        public CreateOrderPage(SkapaOrderViewModel viewModel)
+        public CreateOrderPage(SkapaOrderViewModel viewModel, IKundService kundService)
         {
             InitializeComponent();
 
+            _kundService = kundService;
 
             this.DataContext = viewModel;
         }
+        private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            var window = new AddKundWindow();
+            if (window.ShowDialog() == true)
+            {
+                _kundService.AddKund(window.CreatedKund);
+                MessageBox.Show("Kund tillagd!");//hej
+            }
+        }
+
     }
 }

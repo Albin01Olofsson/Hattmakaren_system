d361f521e389be683c22d7f6fd74d1292222c429
Albin01Olofsson <albin01olofsson@gmail.com>
2026-04-16 14:39:23 +0200

    FIXFIX

---
+++ BL/Interfaces/IAnvändarService.cs
@@ -0,0 +1,14 @@
+﻿using Models;
+
+namespace BL.Interfaces
+{
+    public interface IAnvändarService
+    {
+        public List<Användare> HämtaAllaAnvändare();
+        public Användare HämtaAnvändareMedId(int id);
+        void LäggTillAnvändare(Användare användare);
+        void UpdateraAnvändare(Användare användare);
+        void InaktiveraAnvändare(int id);
+        void TaBortAnvändare(int id);
+    }
+}
---
+++ BL/Services/AnvändarService.cs
@@ -0,0 +1,69 @@
+﻿using BL.Interfaces;
+using DAL.Intefaces;
+using Models;
+
+namespace BL.Services
+{
+    public class AnvändarService : IAnvändarService
+    {
+        private readonly IAnvändarRepo _användarRepo;
+
+        public AnvändarService(IAnvändarRepo användarRepo)
+        {
+            _användarRepo = användarRepo;
+        }
+        public List<Användare> HämtaAllaAnvändare()
+        {
+            return _användarRepo.GetAll();
+        }
+
+        // Metod för att hämta den som är inloggad eller en specifik användare
+        public Användare HämtaAnvändareMedId(int id)
+        {
+            return _användarRepo.GetById(id);
+        }
+
+        public void LäggTillAnvändare(Användare användare)
+        {
+            if (användare == null)
+            {
+                throw new ArgumentNullException(nameof(användare));
+            }
+            // Validera att användarens namn inte är null eller tomt
+            if (string.IsNullOrWhiteSpace(användare.Namn))
+            {
+                throw new ArgumentException("Användarens namn kan inte vara tomt.", nameof(användare));
+            }
+            användare.IsActive = true; // Sätt användaren som aktiv när den läggs till
+            _användarRepo.Add(användare);
+            _användarRepo.Save();
+        }
+
+        public void UpdateraAnvändare(Användare användare)
+        {
+            _användarRepo.Update(användare);
+            _användarRepo.Save();
+        }
+
+        public void InaktiveraAnvändare(int id)
+        {
+            var användare = _användarRepo.GetById(id);
+
+            if (användare == null)
+            {
+                throw new Exception("Användare finns inte!");
+            }
+            användare.IsActive = false;
+            _användarRepo.Update(användare);
+            _användarRepo.Save();
+
+        }
+
+        public void TaBortAnvändare(int id)
+        {
+            _användarRepo.Delete(id);
+            _användarRepo.Save();
+        }
+
+    }
+}

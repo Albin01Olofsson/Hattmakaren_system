3522635e68afd86693a9c42c046a524562828cf0
kanatdavid <david.kanat@hotmail.com>
2026-04-16 11:52:35 +0200

    ladda ned pdf går

---
+++ BL/Services/OrderPdfService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using iText.Kernel.Pdf;
+using iText.Layout;
+using iText.Layout.Element;
+
+namespace BL.Services
+{
+    public class OrderPdfService
+    {
+        public void CreatePdf()
+        {
+
+            Document dokument = new Document(new PdfDocument(new PdfWriter("Order.pdf")));
+            dokument.Add(new Paragraph("Hejsan! Lyckades."));
+            dokument.Close();
+        }
+    }
+}
--- WpfApp1/Views1/OrderBeskrivningPage.xaml.cs
+++ WpfApp1/Views1/OrderBeskrivningPage.xaml.cs
@@ -1,7 +1,8 @@
 ﻿using BL.Interfaces;
 using BL.Services;
-using DAL.Repositorys;
 using DAL;
+using DAL.Repositorys;
+using iText.Kernel.Pdf;
 using Models;
 using System;
 using System.Collections.Generic;
@@ -18,6 +19,9 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using WpfApp1.ViewModels;
+using iText.Kernel.Pdf;
+using iText.Layout;
+using iText.Layout.Element;
 
 namespace WpfApp1.Views1
 {
@@ -26,11 +30,24 @@ namespace WpfApp1.Views1
     /// </summary>
     public partial class OrderBeskrivningPage : Page
     {
+        private Order order;
         public OrderBeskrivningPage(Order o)
         {
             InitializeComponent();
             IProduktService produktService = new ProduktService(new ProduktRepo(new DBcontext()));
             DataContext = new OrderBeskrivningVM(o, produktService);
+            order = o;
 ;       }
+
+        private void LaddaNerPdfKnapp_Click(object sender, RoutedEventArgs e)
+        {
+
+            string fileName = $"Order_{order.OrderID}.pdf";
+
+            Document dokument = new Document(new PdfDocument(new PdfWriter(fileName)));
+            dokument.Close();
+
+            MessageBox.Show($"En PDF har skapats med filnamnet: {fileName}");
+        }
     }
 }

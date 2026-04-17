689bf04c780d75a21f983238fc87eacb43d2d0ce
kanatdavid <david.kanat@hotmail.com>
2026-04-17 11:34:45 +0200

    uppdaterad ui

---
+++ WpfApp1/ViewModels/SpcBestOrderPageVM.cs
@@ -0,0 +1,21 @@
+﻿using BL.Interfaces;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1.ViewModels
+{
+    public partial class SpcBestOrderPageVM : ObservableObject
+    {
+        private IOrderService _orderService;
+        private IProduktService _produktService;
+        public SpcBestOrderPageVM(IOrderService orderService, IProduktService produktService)
+        {
+            _orderService = orderService;
+            _produktService = produktService;
+        }
+    }
+}

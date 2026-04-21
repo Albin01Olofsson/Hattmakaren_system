7c18e5e7698ff4ab222c0d317f2d05837e503c85
kanatdavid <david.kanat@hotmail.com>
2026-04-21 11:19:57 +0200

    fixad med material för Lagerförd Produkt

--- BL/Interfaces/IProduktService.cs
+++ BL/Interfaces/IProduktService.cs
@@ -7,7 +7,7 @@ namespace BL.Interfaces
         Task<List<Produkt>> GetProdukt();
         Task<List<Produkt>> GetProdukter(); //Med Include
         Task<Produkt> GetProduktId(int id);
-        Task AddProdukt(Produkt p);
+        Task AddProdukt(Produkt p, List<int> materialIdn);
         Task AddSpecialBeställning(SpecialBeställning sb, List<int> materialIdn);
         Task UpdateProdukt(Produkt p);
         Task DeleteProdukt(int id);

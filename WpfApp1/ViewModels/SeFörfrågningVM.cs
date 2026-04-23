b92122b52d202a00d23bce4664d3a164370c5bad
kanatdavid <david.kanat@hotmail.com>
2026-04-23 11:57:31 +0200

    ui förfrågan

--- WpfApp1/ViewModels/SeFörfrågningVM.cs
+++ WpfApp1/ViewModels/SeFörfrågningVM.cs
@@ -1,16 +1,21 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Models;
 
 namespace WpfApp1.ViewModels
 {
-    class SeFörfrågningVM
+    public partial class SeFörfrågningVM : ObservableObject
     {
-        public SeFörfrågningVM()
+        [ObservableProperty]
+        private String avsändare = string.Empty;
+
+        public SeFörfrågningVM(Mail m)
         {
-            
+            Avsändare = m.Avsändare;
         }
     }
 }

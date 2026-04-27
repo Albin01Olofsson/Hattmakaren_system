21ec655e2217d0e755c9f93eeb474b02883ede4c
kanatdavid <david.kanat@hotmail.com>
2026-04-27 13:35:50 +0200

    fixxat så de öppnar rätt mapp

--- WpfApp1/Views1/SpcBestOrderPage.xaml.cs
+++ WpfApp1/Views1/SpcBestOrderPage.xaml.cs
@@ -15,6 +15,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using WpfApp1.ViewModels;
+using System.IO;
 
 namespace WpfApp1.Views1
 {
@@ -34,6 +35,9 @@ namespace WpfApp1.Views1
             OpenFileDialog fileDialog = new OpenFileDialog();
 
             fileDialog.Filter = "Tillåtna filnamn.Extensions (*.png;*.jpg;*.jpeg)|*.png;*.jpg;*.jpeg";
+            fileDialog.InitialDirectory = System.IO.Path.Combine(Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory)!.Parent!.Parent!.Parent!.Parent!.FullName, "DAL", "FörfråganBilder");
+
+
 
             if (fileDialog.ShowDialog() == true)
             {
